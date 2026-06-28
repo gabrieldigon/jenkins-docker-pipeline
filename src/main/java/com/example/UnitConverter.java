968c0927e26e2c9d0d5eaaae59f13bf5002f35b0
Gabriel Dias Gonçalves <gabrieldiasgonca@gmail.com>
2026-06-28 14:10:33 -0300

    coonsertando para build normal

--- src/main/java/com/example/UnitConverter.java
+++ src/main/java/com/example/UnitConverter.java
@@ -3,7 +3,7 @@ package com.example;
 public class UnitConverter {
 
     public double kilometersToMiles(double kilometers) {
-        return kilometers * 0.112321371;
+        return kilometers * 0.621371;
     }
 
     public double inchesToCentimeters(double inches) {
